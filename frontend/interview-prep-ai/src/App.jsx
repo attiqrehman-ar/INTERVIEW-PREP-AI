d3e2bf9519713951f1a11bf1ddfb146c2f41454c
attiq <macbook@Macbooks-MacBook-Pro.local>
2025-05-21 17:29:56 +0500

    add routes to the pages

--- frontend/interview-prep-ai/src/App.jsx
+++ frontend/interview-prep-ai/src/App.jsx
@@ -1,9 +1,26 @@
 
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+import LandingPage from "./pages/LandinPage";
+import Dashboard from "./pages/Home/Dashboard";
+import InterviewPrep from "./pages/InterviewPrep/InterviewPrep";
 function App() {
   return (
-    <div className='bg-zinc-900'>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={< LandingPage />} />
+          <Route path='/Login' element={< Login />} />
+          <Route path='/SignUp' element={< Signup />} />
+          <Route path='/dashboard' element={< Dashboard />} />
+          <Route path='/interview-prep/:sessionId' element={< InterviewPrep />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
