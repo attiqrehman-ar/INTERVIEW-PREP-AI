e6a32cab21a2b52d4a48f414d10fbf3c6defd8b2
attiq <macbook@Macbooks-MacBook-Pro.local>
2025-05-23 00:10:02 +0500

    implement profile photo selector for signup popup

--- frontend/interview-prep-ai/src/components/Inputs/ProfilePhoteSelector.jsx
+++ frontend/interview-prep-ai/src/components/Inputs/ProfilePhoteSelector.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import{LuUser, LuUpload, LuTrash } from "react-icons/lu";
 
+
 const ProfilePhoteSelector = ({ image, setImage, preview, setPreview}) => {
+
+    const inputRef= useRef(null);
+    const [previewUrl, setPreviewUrl] =useState(null);
+
+    const handleImageChange =(event)=>{
+        const file =event.target.files[0];
+        if(file){
+            setImage(file);
+            const preview =URL.createObjectURL(file);
+            if(setPreview){
+                setPreview(preview)
+            }
+            setPreviewUrl(preview);
+        }
+    };
+    // const handleRemoveImage=
   return (
     <div>ProfilePhoteSelector</div>
   )
