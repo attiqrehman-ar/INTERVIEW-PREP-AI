dec5309452e63e1a26a9110e320e6d01b3cf3821
attiq <macbook@Macbooks-MacBook-Pro.local>
2025-05-23 22:10:47 +0500

    fix error on server start

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,20 @@ const app = express();
 
 //middleware to handle cors
 
-app.use  {
+app.use{
     cors({
         origin:"*",
         methods:["GET","POST","PUT","DELETE"],
         allowedHeaders:["Content-Type","Authorization"],
-    })
+    });
 };
 
 // Middleware
 app.use(express.json());
+
+// server uploads folder
+app.use("/uploads", express.static(path.join(__dirname,"uploads"),{}));
+
+//start server 
+const PORT= process.env.PORT || 5000;
+app.listen(PORT,()=>console.log(`server running on port ${PORT}`));
